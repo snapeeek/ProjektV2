64b3f52a7a64a592a23e093c2f47e7963dca44b3
snapeeek <maciekwozniak97@gmail.com>
2019-08-30 10:04:10 +0200

    Saving v1.0

--- src/projekt/Saving.java
+++ src/projekt/Saving.java
@@ -1,5 +1,11 @@
 package projekt;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
@@ -29,16 +35,49 @@ public class Saving implements Runnable
         try
         {
             Thread.sleep(FewSeconds());
+            FileOutputStream fos;
+
+            if (folder == null)
+            {
+                fos = new FileOutputStream(sauce + "\\" + filename);
+            }
+            else
+            {
+                fos = new FileOutputStream(sauce +  folder.toString() + "\\" + filename);
+            }
+
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            bos.write(filecontent, 0, filecontent.length);
+            bos.close();
+            fos.close();
+
+            if (folder != null)
+                UpdateCSV();
         }
         catch (InterruptedException IntExp)
         {
             IntExp.printStackTrace();
         }
+        catch (IOException IOExp)
+        {
+            IOExp.printStackTrace();
+        }
     }
 
+    public void UpdateCSV()
+    {
+        while (sem.tryAcquire());
+        try
+        {
+            Files.write(Paths.get("C:\\Users\\mwozn\\Desktop\\FolProj\\Server" + folder.toString() + "\\content.csv"), (filename + "," + username + "\n").getBytes(), StandardOpenOption.APPEND);
+        }
+        catch (IOException IOExp)
+        {
+            IOExp.printStackTrace();
+        }
 
-
-
+        sem.release();
+    }
 
     private int FewSeconds()
     {
