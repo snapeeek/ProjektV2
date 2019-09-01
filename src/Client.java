9ee4b3f5709346bebae8a2ca9bf3b5ab008e6114
snapeeek <maciekwozniak97@gmail.com>
2019-09-01 15:01:15 +0200

    Client v0.2

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,12 @@
 import projekt.*;
 
+import javax.naming.Name;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class Client
@@ -40,6 +43,42 @@ public class Client
 
     protected void NameVerification() throws NameAlreadyTakenException
     {
+        try
+        {
+            dos.writeUTF(username);
+            String ret = dis.readUTF();
+            System.out.println(ret);
+
+            if (ret.compareTo("chuj") != 0)
+                throw new NameAlreadyTakenException();
+        }
+        catch (IOException IOExp)
+        {
+            IOExp.printStackTrace();
+        }
+    }
+
+    protected void run()
+    {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        try
+        {
+            folder = new FolderLocale(path);
+            try
+            {
+                NameVerification();
+            }
+            catch (NameAlreadyTakenException NATExp)
+            {
+                System.out.println(NATExp.GetWarning());
+            }
+        }
+
+
+
+
+
+
 
     }
 }
